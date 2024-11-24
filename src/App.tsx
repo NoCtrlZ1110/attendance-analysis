0a8ac84b889fb16f958f79c891f8c2d952091dfb
Nguyễn Văn Huy <huynd.nguyen@gmail.com>
2024-11-25 01:50:02 +0700

    ui: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
-import { useEffect, useState } from 'react';
+import '@mantine/core/styles.css';
 import './index.css';
+
+import {
+  Container,
+  LoadingOverlay,
+  MantineProvider,
+  Table,
+  Textarea,
+  createTheme,
+} from '@mantine/core';
+import { analyze, getValidDuration, round, scan } from './utils';
+import { useEffect, useState } from 'react';
+
+import { Analysis } from './type';
+import { LineChart } from '@mantine/charts';
+import { SAMPLE_LOG } from './constant';
 import dayjs from 'dayjs';
 import minMax from 'dayjs/plugin/minMax';
 import timezone from 'dayjs/plugin/timezone';
+import { useDisclosure } from '@mantine/hooks';
 import utc from 'dayjs/plugin/utc';
-import { SAMPLE_LOG } from './constant';
-import { analyze, getValidDuration, round, scan } from './utils';
-import { Analysis } from './type';
 
 dayjs.extend(minMax);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault(dayjs.tz.guess());
 
+const theme = createTheme({});
+
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
   const [rawData, setRawData] = useState(SAMPLE_LOG);
   const [dataByDate, setDataByDate] = useState<{ [day: string]: Date[] }>({});
   const [data, setData] = useState<{ [day: string]: number }>();
-  const [analysis, setAnalysis] = useState<Analysis>();
+  const [analysis, setAnalysis] = useState<Analysis>({});
   const dateKeys = Object.keys(dataByDate);
+  const [visible, { toggle }] = useDisclosure(true);
+  const timeSeries = dateKeys.map((key) => ({
+    date: key,
+    duration: analysis.data?.[key],
+  }));
 
   const main = (raw: string) => {
+    setLoading(true);
     const { resultByDay } = scan(raw);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
     const result = analyze(resultByDay);
     setAnalysis(result);
     setDataByDate(resultByDay);
@@ -33,33 +58,106 @@ export default function App() {
   }, [rawData]);
 
   return (
-    <div className='App'>
-      <h1>Attendance Analysis</h1>
-      <h2>Paste your logs here</h2>
-      <textarea value={rawData} onChange={(e) => setRawData(e.target.value)} />
-      <h2>Analysis</h2>
-      <div>
-        <div>Full count: {analysis?.fullCount}</div>
-        <div>Late count: {analysis?.lateCount}</div>
-        <div>Total days: {analysis?.totalDays}</div>
-        <div>Late percentage: {round(analysis?.latePercentage)}%</div>
-      </div>
-      <h2>Details</h2>
+    <MantineProvider theme={theme}>
+      <Container size='md'>
+        <LoadingOverlay visible={isLoading} />
+        <h1>Attendance Analysis</h1>
 
-      {Object.keys(dataByDate).map((key) => (
-        <div key={key} style={{ marginBottom: '1em' }}>
-          <div style={{ marginBottom: '0.5em' }}>
-            <strong>
-              {key} ({round(getValidDuration(dataByDate, key))}h)
-            </strong>
+        <h2>Paste your logs here</h2>
+        <Textarea
+          value={rawData}
+          onChange={(e) => setRawData(e.target.value)}
+          autosize
+          maxRows={10}
+        />
+        <h2>
+          Analysis ({dateKeys.length} days from {dateKeys[0]} to{' '}
+          {dateKeys[dateKeys.length - 1]})
+        </h2>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <div style={{ fontSize: '12px' }}>Total days</div>
+            <div style={{ fontSize: '24px', fontWeight: 'bold' }}>
+              {analysis?.totalDays}
+            </div>
           </div>
           <div>
-            {dataByDate[key].map((d) => (
-              <div>{dayjs(d).format('DD/MM/YYYY - HH:mm')}</div>
-            ))}
+            <div style={{ fontSize: '12px' }}>Full count</div>
+            <div style={{ fontSize: '24px', fontWeight: 'bold' }}>
+              {analysis?.fullCount}
+            </div>
+          </div>
+          <div>
+            <div style={{ fontSize: '12px' }}>Late count</div>
+            <div style={{ fontSize: '24px', fontWeight: 'bold' }}>
+              {analysis?.lateCount}
+            </div>
+          </div>
+
+          <div>
+            <div style={{ fontSize: '12px' }}>Late percentage</div>
+
+            <div
+              style={{
+                fontSize: '24px',
+                fontWeight: 'bold',
+                color:
+                  analysis?.latePercentage > 20
+                    ? 'red'
+                    : analysis?.latePercentage > 15
+                    ? 'yellow'
+                    : 'green',
+              }}
+            >
+              {round(analysis?.latePercentage)}%
+            </div>
           </div>
         </div>
-      ))}
-    </div>
+        <h2>Details</h2>
+
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>STT</Table.Th>
+              <Table.Th>Date</Table.Th>
+              <Table.Th>Duration</Table.Th>
+              <Table.Th>Records</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {Object.keys(dataByDate).map((key, index) => (
+              <Table.Tr
+                key={key}
+                style={{
+                  color:
+                    analysis.data?.[key] === 8
+                      ? 'green'
+                      : analysis.data?.[key] > 7
+                      ? '#df9a3a'
+                      : 'red',
+                }}
+              >
+                <Table.Td>
+                  <strong>{index + 1}</strong>
+                </Table.Td>
+                <Table.Td>
+                  <strong>{key}</strong>
+                </Table.Td>
+                <Table.Td>
+                  <strong>{round(analysis.data?.[key])}</strong>
+                </Table.Td>
+                <Table.Td>
+                  {dataByDate[key].map((d, index) => (
+                    <div key={index}>
+                      {dayjs(d).format('DD/MM/YYYY - HH:mm')}
+                    </div>
+                  ))}
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Container>
+    </MantineProvider>
   );
 }
